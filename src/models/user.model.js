92716c9b7981475c68823879709a28439539b406
Aryan-561 <aryanmail561@gmail.com>
2025-12-04 19:53:12 +0530

    feat: create user model with schema for user attributes

---
+++ src/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+
+    name:{
+        type:String,
+        required:true
+    },
+
+    email:{
+        type:String,
+        required:true,
+    },
+
+    role:{
+        type:[String],
+        enum:['student','admin','teacher'],
+        default:['student'],
+        required: true,
+    },
+
+    enrollment:{
+        type:String,
+    },
+
+    isAdmin:{
+        type:Boolean,
+        default:false,
+        immutable: true,
+    }
+
+})
+
+
+export const User = mongoose.model('User',userSchema);
